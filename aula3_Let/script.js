f747ed4f5490a163c8ceaf71e8a6d58aa3ea6494
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-09 15:32:50 -0300

    Create script.js

---
+++ aula3_Let/script.js
@@ -0,0 +1,20 @@
+// Não podemos criar variáveis com palavras reservadas ex: let let;
+// Dica: Variáveis precisam ter nomes significativos ex: let nome = 'joao';
+// Não pode começar o nome de uma variável com um número.
+// Nome de Variavel não pode conter espaços ou traços. Use: camelCase
+// Ex: nomeCompleto, nomeCliente, meuNome.
+// Case-sensitive = letra minuscula e maiuscula faz diferença.
+// NÃO UTILIZE VAR, USE LET.
+
+let ; // Declarou a Variável
+let nome = 'João';
+var nomeM = 'Maria';
+a = 'Qualquer Valor'; // Inicializando a variável
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu', nomeM );
+console.log(nome, 'casou-se com', nomeM, 'em 2012.');
+console.log(a, 'nasceu em 1984.');
+
+a = 'Josias'
+console.log(a, 'nasceu em 1984.');
