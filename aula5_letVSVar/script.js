33124ab93edbe3f937fbef878bf488d39900ee5b
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-09 15:33:53 -0300

    Create script.js

---
+++ aula5_letVSVar/script.js
@@ -0,0 +1,8 @@
+var nome = 'Luiz';
+var nome = 'Otávio'; // VAR pode ser redeclarado o LET não.
+
+let nomeN = 'Luan';
+nomeN = 'Lucca'; // Maneira correta!
+
+console.log(nome);
+console.log(nomeN);
