cc3b6f3ae64eb8fbc49bb879f9ebddb30177b6d0
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-10 17:32:15 -0300

    exercicio

---
+++ exercicio_ContaNav/script.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Digite um numero: ');
+let num2 = prompt('Digite outro numero: ');
+
+num1 = parseInt(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+
+alert('O Resultado de ' + num1 + ' + ' + num2 + ' é: ' + resultado);
