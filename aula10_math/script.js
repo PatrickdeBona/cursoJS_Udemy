0ba7425ec3126f2d3bc72534607279fc8e748520
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-11 19:01:05 -0300

    Create script.js

---
+++ aula10_math/script.js
@@ -0,0 +1,18 @@
+let num1 = 9.258;
+//let num2 = Math.floor(num1); Arredondando para baixo
+//let num2 = Math.ceil(num1);  Arredonda para cima
+//let num2 = Math.round(num1) Arredonda para o numero mais proximo
+
+// console.log(Math.max(1,2,3,4,5,-10,-50,200,8));Pega o maior numero da sequencia
+// console.log(Math.min(1,2,3,4,5,-10,-50,200,8)); Pega o menor valor
+//console.log(Math.random()); numero randomico entre 0 e 1
+
+const aleatorio = Math.round(Math.random()*(10 - 5) + 5); 
+// numero aleatotio entre 10 e 5, Math.round para tirar as casas decimais
+console.log(aleatorio);
+
+console.log(Math.PI);
+console.log(Math.pow(2,10)); // 2 ** 10
+
+let numm = 9;
+console.log(numm ** (1/2)); // faz a raiz
