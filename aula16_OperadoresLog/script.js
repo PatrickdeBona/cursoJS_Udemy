81c73f32bfe6dbac23b8b0c0f316b6878114fb2b
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-14 22:36:55 -0300

    Create script.js

---
+++ aula16_OperadoresLog/script.js
@@ -0,0 +1,16 @@
+/*
+Operadores Lógicos
+&& -> AND
+|| -> OR
+!  -> NOT
+
+FALSY:
+*false
+0
+' '|" "|` `
+null/ undefined
+NaN 
+*/
+
+console.log(0 && "Lucca"); //falso
+console.log(1 && "Davi"); //Verdadeiro
