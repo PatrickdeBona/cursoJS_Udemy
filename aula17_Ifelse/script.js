d5b6e30cb2ae4ae9b65ba63a546ad7895f75b964
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-14 22:37:15 -0300

    Create script.js

---
+++ aula17_Ifelse/script.js
@@ -0,0 +1,16 @@
+/*
+if -> se
+else if -> senão
+else -> ou*
+*/
+
+const hora = 10;
+if (hora >= 0  && hora <= 11) {
+    console.log('Bom dia');
+} else if (hora >= 12 && hora <= 17){
+    console.log('Boa tarde');
+} else if ( hora >= 18 && hora <= 23) {
+    console.log('Boa noite');
+} else {
+    console.log('Hora não existente!')
+}
