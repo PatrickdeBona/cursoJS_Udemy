180b5a2ab44857a7b9144dad844f4253ae775cc2
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-14 22:36:16 -0300

    Create script.js

---
+++ aula15_OperadoresComp/script.js
@@ -0,0 +1,13 @@
+/*
+Operadores de Comparação:
+> maior que
+>= maior ou igual a
+< menor que
+<= menor ou igual a
+== igualdade (valor) **** Nao recomendado
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) ****** Nao recomendado
+!== diferente estrito(valor e tipo)
+*/
+const comp = 10 < 5;
+console.log(comp);
