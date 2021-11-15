d996a4ac8e6210014737e59b4031ae9102b77078
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-15 20:50:49 -0300

    Create script.js

---
+++ aula18_OperadorTernario/script.js
@@ -0,0 +1,15 @@
+// (condição) ?  'Valor para Verdadeiro' : 'Valor para falso';
+const pontuacao = 999;
+const nivelUsuario = pontuacao >= 1000 ? 'Usuario Vip' : 'Usuario Normal';
+console.log(nivelUsuario);
+
+//----------------------------------------------------------------------------
+/* if (pontuacao >= 1000){
+     console.log('User vip');
+    }else {
+        console.log('User Normal');
+    }
+}
+*/
+
+// Os dois são iguais!
