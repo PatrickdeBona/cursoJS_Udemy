4c2913a108bfebe08df4e3444b17b7e580a72ada
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-18 21:34:04 -0300

    Create script.js

---
+++ aula24_For/script.js
@@ -0,0 +1,16 @@
+/*
+Console.log('linha 1);
+Console.log('linha 2);
+Console.log('linha 3);
+Console.log('linha 4);
+*/
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`linha ${i}`)
+}
+
+const frutas = ['Maça', 'Pêra', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++){
+    console.log(frutas[i]);
+}
