46d5a02e6513de0c4be971be5b1ea411dd5d1376
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-25 21:01:57 -0300

    Create script.js

---
+++ aula36_FuncaoImediata/script.js
@@ -0,0 +1,11 @@
+//IIFE -> Immediately invoked function expression
+(function() {
+    console.log(123456789);
+})();
+
+/*
+|Não toca o escopo global
+|Criada e executada junto
+|Protege o código
+|Funciona de forma normal
+*/
