0470b14268903d9f2b123a484a541322672ad64b
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-26 21:37:13 -0300

    Create script.js

---
+++ aula43_ConcatenandoArrays/script.js
@@ -0,0 +1,7 @@
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+const num3 = num1.concat(num2); //concatena
+//... spread
+const num4 = [...num1,...num2]; //concatena
+console.log(num3);
+console.log(num4);
